089db036a5d53f0cccf856214c5065f4cf5012a9
Christian <christian_mahardhika@telkomsel.co.id>
2024-04-26 21:15:33 +0700

    Refactor ticket usecases and add unit tests

--- internal/module/ticket/usecases/usecases_test.go
+++ internal/module/ticket/usecases/usecases_test.go
@@ -11,17 +11,14 @@ import (
 	"ticket-service/internal/module/ticket/usecases"
 	"time"
 
-	"github.com/ThreeDotsLabs/watermill/message"
-	"github.com/gorules/zen-go"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 var (
-	uc                   usecases.Usecases
-	repo                 *mocks.Repositories
-	ctx                  context.Context
-	publish              message.Publisher
-	onlineTicketRulesBre zen.Decision
+	uc   usecases.Usecases
+	repo *mocks.Repositories
+	ctx  context.Context
 )
 
 func setup() {
@@ -232,3 +229,171 @@ func TestIncrementTicketStock(t *testing.T) {
 		require.NoError(t, err)
 	})
 }
+
+func TestCheckStockTicket(t *testing.T) {
+	// Setup
+	setup()
+	defer repo.AssertExpectations(t)
+
+	t.Run("success", func(t *testing.T) {
+		ticketDetailID := 1
+
+		// Mock repository calls
+		ticketDetail := entity.TicketDetail{
+			ID:        int64(ticketDetailID),
+			TicketID:  1,
+			Level:     "exampleLevel",
+			Stock:     10,
+			BasePrice: 100.00,
+			CreatedAt: time.Time{},
+			UpdatedAt: sql.NullTime{},
+			DeletedAt: sql.NullTime{},
+		}
+
+		repo.On("FindTicketDetail", ctx, int64(ticketDetailID)).Return(ticketDetail, nil)
+
+		// Call the function
+		result, err := uc.CheckStockTicket(ctx, ticketDetailID)
+
+		// Assertions
+		require.NoError(t, err)
+		require.Equal(t, response.StockTicket{Stock: 10}, result)
+	})
+}
+
+func TestInquiryTicketAmount(t *testing.T) {
+	// Setup
+	setup()
+	defer repo.AssertExpectations(t)
+
+	t.Run("success", func(t *testing.T) {
+		ticketID := int64(1)
+		totalTicket := 5
+
+		// Mock repository calls
+		ticket := entity.Ticket{
+			ID:        ticketID,
+			Capacity:  100,
+			Region:    "exampleRegion",
+			EventDate: time.Now(),
+			CreatedAt: time.Time{},
+			UpdatedAt: sql.NullTime{},
+			DeletedAt: sql.NullTime{},
+		}
+
+		ticketDetails := []entity.TicketDetail{
+			{
+				ID:        1,
+				TicketID:  ticket.ID,
+				Level:     "exampleLevel",
+				Stock:     10,
+				BasePrice: 100.00,
+				CreatedAt: time.Time{},
+				UpdatedAt: sql.NullTime{},
+				DeletedAt: sql.NullTime{},
+			},
+			{
+				ID:        2,
+				TicketID:  ticket.ID,
+				Level:     "exampleLevel",
+				Stock:     10,
+				BasePrice: 100.00,
+				CreatedAt: time.Time{},
+				UpdatedAt: sql.NullTime{},
+				DeletedAt: sql.NullTime{},
+			},
+			{
+				ID:        3,
+				TicketID:  ticket.ID,
+				Level:     "exampleLevel",
+				Stock:     10,
+				BasePrice: 100.00,
+				CreatedAt: time.Time{},
+				UpdatedAt: sql.NullTime{},
+				DeletedAt: sql.NullTime{},
+			},
+		}
+
+		repo.On("FindTicketDetail", ctx, ticketDetails[0].ID).Return(ticketDetails[0], nil)
+
+		// Call the function
+		result, err := uc.InquiryTicketAmount(ctx, ticketID, totalTicket)
+
+		// Assertions
+		require.NoError(t, err)
+		require.Equal(t, response.InquiryTicketAmount{
+			TotalTicket: 5,
+			TotalAmount: 500.00,
+		}, result)
+	})
+
+	t.Run("error find ticket", func(t *testing.T) {
+		ticketID := int64(2)
+		totalTicket := 5
+
+		// Mock repository calls
+		repo.On("FindTicketDetail", ctx, ticketID).Return(entity.TicketDetail{}, errors.New("error"))
+
+		// Call the function
+		result, err := uc.InquiryTicketAmount(ctx, ticketID, totalTicket)
+
+		// Assertions
+		require.Error(t, err)
+		assert.Equal(t, response.InquiryTicketAmount{}, result)
+	})
+}
+
+// func TestShowTickets(t *testing.T) {
+// 	// Setup
+// 	setup()
+// 	defer repo.AssertExpectations(t)
+
+// 	t.Run("success", func(t *testing.T) {
+// 		// Mock repository calls
+// 		tickets := []entity.Ticket{
+// 			{
+// 				ID:        1,
+// 				Capacity:  100,
+// 				Region:    "exampleRegion",
+// 				EventDate: time.Now(),
+// 				CreatedAt: time.Time{},
+// 				UpdatedAt: sql.NullTime{},
+// 				DeletedAt: sql.NullTime{},
+// 			},
+// 			{
+// 				ID:        2,
+// 				Capacity:  100,
+// 				Region:    "exampleRegion",
+// 				EventDate: time.Now(),
+// 				CreatedAt: time.Time{},
+// 				UpdatedAt: sql.NullTime{},
+// 				DeletedAt: sql.NullTime{},
+// 			},
+// 		}
+// 		page := 1
+// 		pageSize := 10
+
+// 		repo.On("FindAllTickets", ctx).Return(tickets, nil)
+
+// 		// Call the function
+// 		result, _, _, err := uc.ShowTickets(ctx, page, pageSize, 1)
+
+// 		// Assertions
+// 		require.NoError(t, err)
+// 		require.Len(t, result, 2)
+// 	})
+
+// 	t.Run("error", func(t *testing.T) {
+// 		page := 1
+// 		pageSize := 10
+// 		// Mock repository calls
+// 		repo.On("FindAllTickets", ctx).Return([]entity.Ticket{}, errors.New("error"))
+
+// 		// Call the function
+// 		result, _, _, err := uc.ShowTickets(ctx, page, pageSize, 2)
+
+// 		// Assertions
+// 		require.Error(t, err)
+// 		assert.Nil(t, result)
+// 	})
+// }
